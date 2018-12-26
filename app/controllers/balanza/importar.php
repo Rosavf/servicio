a627cdf73e0aa5a00a8fda41dfea9104d620ee52
HardCoreWare <bernardocodu@gmail.com>
2018-12-26 14:05:32 -0600

    listo

--- app/controllers/balanza/importar.php
+++ app/controllers/balanza/importar.php
@@ -2,11 +2,40 @@
 
 class importar extends Controller{
 
-    public function mensual(){
+    public function mensual($params){
+
+
+        $paramArray=explode($params);
+
+        $year=$paramArray[0];
+        $month=$paramArray[1];
+
 
         $this->model('endpoints','dataimporter');
-        $dataImporter=new DataImporter();
+        $dataImporter=new DataImporter($year,$month,"BANCO","=");
         $dataImporter=null;
+
+        $this->model('endpoints','dataimporter');
+        $dataImporter=new DataImporter($year,$month,"CASA","=");
+        $dataImporter=null;
+
+        $this->model('endpoints','dataimporter');
+        $dataImporter=new DataImporter($year,$month,"GRUPO","=");
+        $dataImporter=null;
+
+        $this->model('endpoints','dataimporter');
+        $dataImporter=new DataImporter($year,$month,"OPERADORA","=");
+        $dataImporter=null;
+
+        $this->model('endpoints','dataimporter');
+        $dataImporter=new DataImporter($year,$month,"SAVELLA","=");
+        $dataImporter=null;
+
+        $this->model('endpoints','dataimporter');
+        $dataImporter=new DataImporter($year,$month,"SERVICIOS","=");
+        $dataImporter=null;
+
+        echo('success');
 
 
     }
