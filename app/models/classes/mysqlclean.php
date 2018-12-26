033f192c19d0c40768ce8cced646b226cd9bd55d
HardCoreWare <bernardocodu@gmail.com>
2018-12-26 16:18:54 -0600

    listo

---
+++ app/models/classes/mysqlclean.php
@@ -0,0 +1,32 @@
+<?php
+
+class MySqlDataClean{
+
+    protected $mySql;
+
+    public function attachMySql($mySql){
+
+        $this->mySql=$mySql;
+        $this->mySql->begin('localhost','root','Pit2018mtv#@','INFORME');
+
+    }
+
+    //
+    public function detachMySql(){
+
+        $this->mySql=null;
+
+    }
+
+    //
+    public function cleanData($table){
+
+        $this->mySql->truncate($table);
+
+    }
+
+}
+
+
+
+?>
