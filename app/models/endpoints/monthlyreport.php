3ffe13dd1f60fffee2e7bfe9811c22c24f903fc3
HarCoreWare <bernardocodu@gmail.com>
2018-12-26 02:43:49 -0600

    generando lectura

---
+++ app/models/endpoints/monthlyreport.php
@@ -0,0 +1,22 @@
+<?php
+
+class MonthlyReport extends MySqlDataReading{
+
+    public function parseParams($params){
+
+        $paramArray=explode("-",$params);
+
+        $year=$paramArray[0];
+        $month=$paramArray[1];
+        $module=$paramArray[2];
+
+        $this->attachMySql(new PdoCrud());
+        $this->readAccounts("Mensual","BANCO");        
+
+
+    }
+    
+}
+
+
+?>
