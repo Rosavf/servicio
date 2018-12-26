0d2b93b72d10690ecfe8c68d6482b31058d14e0b
HardCoreWare <bernardocodu@gmail.com>
2018-12-26 14:11:49 -0600

    listo

--- app/controllers/balanza/importar.php
+++ app/controllers/balanza/importar.php
@@ -42,7 +42,37 @@ class importar extends Controller{
 
     public function acumulado($params){
 
-                
+        $paramArray=explode("-",$params);
+
+        $year=$paramArray[0];
+        $month=$paramArray[1];
+
+
+        $this->model('endpoints','dataimporter');
+        $dataImporter=new DataImporter($year,$month,"BANCO","<=","Mensual");
+        $dataImporter=null;
+
+        $this->model('endpoints','dataimporter');
+        $dataImporter=new DataImporter($year,$month,"CASA","<=","Mensual");
+        $dataImporter=null;
+
+        $this->model('endpoints','dataimporter');
+        $dataImporter=new DataImporter($year,$month,"GRUPO","<=","Mensual");
+        $dataImporter=null;
+
+        $this->model('endpoints','dataimporter');
+        $dataImporter=new DataImporter($year,$month,"OPERADORA","<=","Mensual");
+        $dataImporter=null;
+
+        $this->model('endpoints','dataimporter');
+        $dataImporter=new DataImporter($year,$month,"SAVELLA","<=","Mensual");
+        $dataImporter=null;
+
+        $this->model('endpoints','dataimporter');
+        $dataImporter=new DataImporter($year,$month,"SERVICIOS","<=","Mensual");
+        $dataImporter=null;
+
+        echo('success');               
 
     }
 
