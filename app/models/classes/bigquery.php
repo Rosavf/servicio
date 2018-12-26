f105b1756ee5890881cea948b93933c143f22f5b
HardCoreWare <bernardocodu@gmail.com>
2018-12-26 15:45:40 -0600

    listo

---
+++ app/models/classes/bigquery.php
@@ -0,0 +1,25 @@
+<?php
+
+class BigQueryAccountImport extends BigQueryConnection implements BigQueryAccountImporting{
+
+    protected $accountArray=[]; //arreglo que guarda las cuentas obtenidas
+
+    //importamos las cuentas desde la tabla de cuentas en BigQuery
+    public function importAccounts($accountTable){
+
+        $dml = 'SELECT ID, HKONT, CONCEPTO, SUPERCONCEPTO FROM '.$accountTable.' ORDER BY ID';
+
+        $accountResult = $this->bigQuery->select($dml);
+
+        foreach ($accountResult as $i => $row) {
+
+            $this->accountArray[$i] = $row;
+
+        }
+
+ 
+    }
+
+}
+
+?>
