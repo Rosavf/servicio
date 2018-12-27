f249cfa206cafa87ab478bb82540c0c303f8ab9f
HardCoreWare <bernardocodu@gmail.com>
2018-12-27 08:25:40 -0600

    ok

--- app/models/classes/mysqldataread.php
+++ app/models/classes/mysqldataread.php
@@ -4,8 +4,8 @@ class MySqlDataRead implements MySqlDataReading{
 
     protected $mySql;
     protected $accountArray=[];   
+    protected $readArray=[];
     protected $dataArray=[];
-    protected $nestedArray=[];
 
     //
     public function attachMySql($mySql){
@@ -32,7 +32,7 @@ class MySqlDataRead implements MySqlDataReading{
         for($i=0;$i<count($this->accountArray);$i++){
 
             $months = $this->mySql->select($table,["Modulo", "Id_Cuenta", "Anualidad", "Concepto", "Super_Concepto", "Mes" ,"Subtotal"],"Modulo = '".$module."'"." AND "."Id_Cuenta = '".$this->accountArray[$i]."'","Mes","assoc");
-
+            
             foreach ($months as $row) {
 
                 $results[]=$row;
@@ -42,12 +42,11 @@ class MySqlDataRead implements MySqlDataReading{
 
         }
 
-        $this->accountArray=null;
+        //
 
-        $this->dataArray['Data']=$results;
+        
 
-        echo(json_encode($this->dataArray));
-
+        
     }
 
 }
