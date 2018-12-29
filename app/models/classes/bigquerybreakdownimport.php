a5435dbc6172e2d9cd62ad7104feca8a233df6a8
HardCoreWare <bernardocodu@gmail.com>
2018-12-28 21:02:12 -0600

    preparando nuevos desgloses

--- app/models/classes/bigquerybreakdownimport.php
+++ app/models/classes/bigquerybreakdownimport.php
@@ -25,7 +25,6 @@ class BigQueryBreakdownImport extends BigQueryDataImport{
         $subquerys=null;
         $this->Array_Desgloses[] = $this->bigQuery->select($uniquery);
         $uniquery=null;
-
         print_r($this->breakdownArray);
 
     }
--- app/models/endpoints/breakdownimporter.php
+++ app/models/endpoints/breakdownimporter.php
@@ -5,6 +5,7 @@ class BreakdownImporter extends BigQueryBreakdownImport{
     public function __construct($year,$month,$module){
 
         $this->attachBigQuery(new BigQuery());
+        $this->importAccounts(ACCOUNTS_TABLE);
         $this->importBreakDown($year,$month,$module,BSEG_TABLE,CECOS_TABLE,ACCOUNTS_TABLE);
         $this->attachBigQuery();
 
