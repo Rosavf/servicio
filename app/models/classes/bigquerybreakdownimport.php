80fc1164b44fa6bae876298557f2dbb43937160b
HardCoreWare <bernardocodu@gmail.com>
2018-12-28 21:34:28 -0600

    preparando nuevos desgloses

--- app/models/classes/bigquerybreakdownimport.php
+++ app/models/classes/bigquerybreakdownimport.php
@@ -8,14 +8,14 @@ class BigQueryBreakdownImport extends BigQueryAccountImport{
 
         $subquerys=[];
 
-        foreach ($this->accountTable as $row) {
+        foreach ($this->accountArray as $row) {
 
             $dml = 'SELECT "'.$module.'" AS MODULO,'.$row['ID'].' AS ID, '.' DMBTR AS MONTO, CAST(SUBSTR(BUDAT,1,4) AS INT64) AS ANUALIDAD, CAST(SUBSTR(BUDAT,5,2) AS INT64) AS MES, BUDAT AS FECHA, PSWSL AS MONEDA, KOSTL AS CECO, BKTXT AS DESCRIPCION FROM'.
             ' '.$bigTable.' '.
             ' WHERE CAST(SUBSTR(BUDAT,5,2) AS INT64) <= '.strval($month).
             ' AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = '.strval($year).
             ' AND KOSTL IN (SELECT KOSTL FROM '.$cecosTable.' WHERE MODULO = "'.strval($module).'")'.
-            ' AND HKONT = (SELECT HKONT FROM '.$accountsTable.' WHERE ID = "'.$row['ID'].'")';
+            ' AND HKONT = (SELECT HKONT FROM '.$accountArray.' WHERE ID = "'.$row['ID'].'")';
 
             $subquerys[]=$dml;
                     
