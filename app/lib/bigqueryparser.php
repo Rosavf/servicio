35df558512b9b6e6979e7e1cdcac3170a58bae15
HardCoreWare <bernardocodu@gmail.com>
2019-01-03 17:42:31 -0600

    ok probando nuevo upgrade

--- app/lib/bigqueryparser.php
+++ app/lib/bigqueryparser.php
@@ -31,7 +31,6 @@ class BigQueryParser extends BigQuery{
 
                     }
 
-
                     switch ($type) {
 
                         case 'assoc': 
@@ -40,8 +39,61 @@ class BigQueryParser extends BigQuery{
 
                             if(is_array($cell1)){
 
+                                $line2=[];
                                 $keys1=array_keys($cell1);
                                 $tipe1;
+
+                                if((count($keys1)==0)||($keys1[0]==0)){
+
+                                    $type1="num";
+            
+                                }
+            
+                                else{
+            
+                                    $type1="assoc";
+            
+                                }
+
+                                switch ($type1) {
+
+                                    case 'num':
+
+                                    foreach ($cell1 as $cell2) {
+
+                                        if(is_array($cell2)){
+
+
+
+                                        }
+
+                                        else{
+
+                                            $line2[]=$cell2;
+
+                                        }
+
+                                    }
+
+
+                                    break;
+
+                                    case 'assoc':
+
+                                    foreach ($cell1 as $key2 => $cell2) {
+
+                                        
+
+                                    }
+
+                                    break;
+                                    
+                                    default:
+
+                                    break;
+                                }
+
+                                $line1[$key1]=$cell1;
 
                             }
 
@@ -61,7 +113,61 @@ class BigQueryParser extends BigQuery{
 
                             if(is_array($cell1)){
 
+                                $line2=[];
+                                $keys1=array_keys($cell1);
+                                $tipe1;
 
+                                if((count($keys1)==0)||($keys1[0]==0)){
+
+                                    $type1="num";
+            
+                                }
+            
+                                else{
+            
+                                    $type1="assoc";
+            
+                                }
+
+                                switch ($type1) {
+
+                                    case 'num':
+
+                                    foreach ($cell1 as $cell2) {
+
+                                        if(is_array($cell2)){
+
+
+
+                                        }
+
+                                        else{
+
+                                            $line2[]=$cell2;
+
+                                        }
+
+                                    }
+
+
+                                    break;
+
+                                    case 'assoc':
+
+                                        foreach ($cell1 as $key2 => $cell2) {
+
+                                            
+
+                                        }
+
+                                    break;
+                                    
+                                    default:
+
+                                    break;
+                                }
+
+                                $line1[]=$cell1;
 
                             }
 
