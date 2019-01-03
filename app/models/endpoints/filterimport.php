e9f571a9eec00721da5d0ff9d86f11b3d3e439ce
HardCoreWare <bernardocodu@gmail.com>
2019-01-03 14:05:39 -0600

    ruta de importacion de cuentas lista, punto final y clase madre lista

--- app/models/endpoints/filterimport.php
+++ app/models/endpoints/filterimport.php
@@ -4,7 +4,9 @@ class FilterImport extends BigQueryFilterImport{
 
     public function __contruct(){
 
+        $this->attachBigQuery(new BigQuery());
         $this->importAccountFilters("CUENTAS");
+        $this->detachBigQuery();
 
     }
 
