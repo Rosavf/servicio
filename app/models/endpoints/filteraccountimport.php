d3dbfb2f383dbc6887cc54d0feba9f08289d29a5
HardCoreWare <bernardocodu@gmail.com>
2019-01-07 10:08:36 -0600

    testeando servicio

--- app/models/endpoints/filteraccountimport.php
+++ app/models/endpoints/filteraccountimport.php
@@ -5,7 +5,7 @@ final class FilterAccountImport extends BigQueryFilterImport{
     public function __construct($module){
 
         $this->attachBigQuery(new BigQueryParser());
-        $this->importAccountFilters("CUENTAS",$module);
+        $this->importAccountFilters("`informe-211921.MULTIVA.CUENTAS`",$module);
         $this->detachBiqQuery();
 
     }
