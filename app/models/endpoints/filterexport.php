598de2611a64a69acdaad62ba6636a6092e888fe
HardCoreWare <bernardocodu@gmail.com>
2019-01-07 11:28:56 -0600

    oki

--- app/models/endpoints/filterexport.php
+++ app/models/endpoints/filterexport.php
@@ -5,15 +5,10 @@ final class FilterExport extends BigQueryFilterExport{
     public function __construct($module,$request){
 
         $this->readPostRequest($module,$request);
+        $this->updateAccountFilters("`informe-211921.MULTIVA.CUENTAS`");
 
     }
 
-    public function updateAccountFilters($table){
-
-        print_r($this->filterArray);
-
-    }
-    
 }
 
 
