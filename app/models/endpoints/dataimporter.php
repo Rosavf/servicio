93d37a84f90a6a15953c2b1febd44ff536c9d48c
HardCoreWare <bernardocodu@gmail.com>
2019-01-07 14:00:30 -0600

    listo

--- app/models/endpoints/dataimporter.php
+++ app/models/endpoints/dataimporter.php
@@ -5,8 +5,8 @@ final class DataImporter extends MySqlDataWrite{
     public function __construct($year,$month,$module,$operator,$sqlTable){
 
         $this->attachBigQuery(new BigQueryParser());
-        $this->importAccounts(ACCOUNTS_TABLE);
-        $this->importData($year,$month,$module,$operator,BSEG_TABLE,CECOS_TABLE);
+        $this->importAccounts('`informe-211921.MULTIVA.CUENTAS`');
+        $this->importData($year,$month,$module,$operator,'`informe-211921.MULTIVA.BSEGAIO`','`informe-211921.MULTIVA.CECOS`');
         $this->detachBiqQuery();
         $this->attachMySql(new PdoCrud());
         $this->writeMySql($sqlTable);
