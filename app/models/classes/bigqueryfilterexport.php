a71b63f424f8209a279b53f05c1a81430372f722
HardCoreWare <bernardocodu@gmail.com>
2019-01-07 11:48:23 -0600

    actualizancion de clausula UPDATE SET en filtros

--- app/models/classes/bigqueryfilterexport.php
+++ app/models/classes/bigqueryfilterexport.php
@@ -31,7 +31,7 @@ class BigQueryFilterExport extends BigQueryConnection{
 
     public function updateAccountFilters($table){
 
-        print_r($this->filterArray);
+        //print_r($this->filterArray);
 
     }
 
