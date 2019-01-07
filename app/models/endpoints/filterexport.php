7072c75e3981fec7038f8300b46748bbda0bb914
HardCoreWare <bernardocodu@gmail.com>
2019-01-07 12:22:25 -0600

    actualizancion de clausula UPDATE SET en filtros

--- app/models/endpoints/filterexport.php
+++ app/models/endpoints/filterexport.php
@@ -5,7 +5,7 @@ final class FilterExport extends BigQueryFilterExport{
     public function __construct($module,$request){
 
         $this->readPostRequest($module,$request);
-        $this->updateAccountFilters("`informe-211921.MULTIVA.CUENTAS`");
+        $this->updateAccountFilters("BANCO","`informe-211921.MULTIVA.CUENTAS`");
 
     }
 
