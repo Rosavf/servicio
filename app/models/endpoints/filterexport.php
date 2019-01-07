a1ed20d3fa052bffa4424ccde8197cb702e9baf7
HardCoreWare <bernardocodu@gmail.com>
2019-01-07 11:27:11 -0600

    oki

--- app/models/endpoints/filterexport.php
+++ app/models/endpoints/filterexport.php
@@ -4,7 +4,13 @@ final class FilterExport extends BigQueryFilterExport{
 
     public function __construct($module,$request){
 
+        $this->readPostRequest($module,$request);
 
+    }
+
+    public function updateAccountFilters($table){
+
+        print_r($this->filterArray);
 
     }
     
