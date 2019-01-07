b93aeddbf7fc2599b0226c2734e148b388b936aa
HardCoreWare <bernardocodu@gmail.com>
2019-01-07 12:04:05 -0600

    actualizancion de clausula UPDATE SET en filtros

--- app/models/classes/bigqueryfilterexport.php
+++ app/models/classes/bigqueryfilterexport.php
@@ -22,8 +22,14 @@ class BigQueryFilterExport extends BigQueryConnection{
                 case false: $filterLine['PAGADO']="FALSE";
                 
                     break;
-                
+
             }
+
+            $accounts=[];
+
+            if($row[1]){$accounts[]="5200";}
+            if($row[2]){$accounts[]="5300";}
+            if($row[3]){$accounts[]="5500";}
 
             $this->filterArray[]=$filterLine;
 
@@ -33,8 +39,10 @@ class BigQueryFilterExport extends BigQueryConnection{
 
     public function updateAccountFilters($table){
 
-        print_r($this->filterArray);
 
+
+
+        
     }
 
 }
