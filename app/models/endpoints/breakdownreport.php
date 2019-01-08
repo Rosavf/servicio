04bda1e5bec09554bac095f84a3b6bd006189676
HardCoreWare <bernardocodu@gmail.com>
2019-01-07 20:34:17 -0600

    listo

--- app/models/endpoints/breakdownreport.php
+++ app/models/endpoints/breakdownreport.php
@@ -4,8 +4,15 @@ final class BreakdownReport extends MySqlBreakdownRead{
 
     public function __construct($table,$params){
 
+        $paramArray=explode("-",$params);
+
+        $year=$paramArray[0];
+        $month=$paramArray[1];
+        $module=$paramArray[2];
+        $id=$paramArray[3];
+
         $this->attachMySql(new PdoCrud());
-        $this->readBreakDown('Desglose','2018','3','BANCO','1');
+        $this->readBreakDown('Desglose',$year,$month,$module,$id);
         $this->detachMySql();
     
     }
