e24959e30655abb45c19695967ec6294a30bbd2c
HardCoreWare <bernardocodu@gmail.com>
2019-01-08 16:48:46 -0600

    probando lectura

--- app/models/classes/mysqlsummaryread.php
+++ app/models/classes/mysqlsummaryread.php
@@ -12,9 +12,7 @@ class MySqlSummaryRead extends MySqlConnection implements MySqlSummaryReading{
 
             $accountArray = $this->mySql->selectDistinct($table,"Id_Cuenta","Modulo = '".$module."'","Id_Cuenta");
 
-            print_r($accountArray);
-
-            for ($i=0; $i < $accountArray; $i++) { 
+            for ($i=0; $i < count($accountArray); $i++) { 
     
                 $moduleResult = $this->mySql->select($table,["Modulo", "Id_Cuenta", "Anualidad", "Concepto", "Super_Concepto","Subtotal"],"Modulo = '".$module."'"." AND "."Id_Cuenta = '".$accountArray[$i]."'","Mes","assoc");
     
