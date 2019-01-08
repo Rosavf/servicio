2773fe442572c9fdb2b362c83b35e7de777b86b8
HardCoreWare <bernardocodu@gmail.com>
2019-01-08 14:52:10 -0600

    nueva ruta para servicios

--- app/controllers/balanza/resumen.php
+++ app/controllers/balanza/resumen.php
@@ -5,6 +5,8 @@ class resumen extends Controller{
     public function mensual($params){
 
         $this->model('endpoints','summary');
+
+        $summary = new Summary('Mensual',$params);
         
     }
 
