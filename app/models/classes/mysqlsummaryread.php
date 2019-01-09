47162928a6b0d829965459691255dbd9aedd14da
HardCoreWare <bernardocodu@gmail.com>
2019-01-09 13:15:48 -0600

    listo

--- app/models/classes/mysqlsummaryread.php
+++ app/models/classes/mysqlsummaryread.php
@@ -5,27 +5,29 @@ class MySqlSummaryRead extends MySqlConnection implements MySqlSummaryReading{
     protected $superconcepts=["FACTOR HUMANO","GASTOS GENERALES"];
     protected $modules=["BANCO","CASA","GRUPO","OPERADORA","SAVELLA","SERVICIOS"];
 
-    //
     public function readModules($table,$month){
 
         $results=[];
 
         foreach ($this->superconcepts as $superconcept) {
 
-            $results["Super_Concepto"] = 
+            $results1=[];
+
+            $results1["Super_Concepto"] = $superconcept;
+
+            $results["Cuentas"]=[];
             
             $accountArray = $this->mySql->selectDistinct($table,"Id_Cuenta","Super_Concepto = '".$superconcept."'","Id_Cuenta");
 
-            for ($i=0; $i < count($accountArray); $i++) { 
-        
-                foreach ($this->modules as $module) {
-    
-                    $moduleResult = $this->mySql->select($table,["Modulo", "Concepto","Subtotal"],"Modulo = '".$module."'"." AND "."Id_Cuenta = '".$accountArray[$i]."'","Mes","assoc");
-                    
-        
-                }
-    
+            foreach ($accountArray as $account) {
+
+                //$results
+
+                //Id_Cuenta
+
             }
+
+            $results[]=$results1;
     
         }
 
