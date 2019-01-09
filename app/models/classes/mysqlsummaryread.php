3d19f50c714e65d1d735a0178be2b4595e7bc28c
HardCoreWare <bernardocodu@gmail.com>
2019-01-09 13:28:53 -0600

    listo

--- app/models/classes/mysqlsummaryread.php
+++ app/models/classes/mysqlsummaryread.php
@@ -21,7 +21,7 @@ class MySqlSummaryRead extends MySqlConnection implements MySqlSummaryReading{
 
             foreach ($accountArray as $account) {
 
-                $results["Cuentas"][]=$account;
+                $results1["Cuentas"][]=$account;
 
             }
 
