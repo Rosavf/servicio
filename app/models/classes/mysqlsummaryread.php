a31a5c111ba4910600461f45bc939e5605049176
HardCoreWare <bernardocodu@gmail.com>
2019-01-09 14:35:34 -0600

    preparando nuevos servicio en resumen

--- app/models/classes/mysqlsummaryread.php
+++ app/models/classes/mysqlsummaryread.php
@@ -29,7 +29,7 @@ class MySqlSummaryRead extends MySqlConnection implements MySqlSummaryReading{
 
                     $conditions="Modulo = '".$module."'"." AND "."Id_Cuenta = '".$account."'"." AND "."Mes = '".$month."'";
 
-                    $mensualidades = $crud->select($table,["Subtotal"],$conditions,"Mes","assoc");
+                    $mensualidades = $this->mySql->select($table,["Subtotal"],$conditions,"Mes","assoc");
 
                 }
 
