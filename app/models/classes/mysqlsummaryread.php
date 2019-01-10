c3c4823b8720635c16fadb92cfe17469e6e2ab64
HardCoreWare <bernardocodu@gmail.com>
2019-01-10 16:39:41 -0600

    preparando nuevos servicio en resumen

--- app/models/classes/mysqlsummaryread.php
+++ app/models/classes/mysqlsummaryread.php
@@ -24,6 +24,7 @@ class MySqlSummaryRead extends MySqlConnection implements MySqlSummaryReading{
 
                 $results2=[];
                 $results2["Id_Cuenta"]=$account;
+                
                 $results2["Cuenta"] = $this->mySql->select($table,["Concepto"],"Id_Cuenta = '".$account."'"." AND "."Mes = '".$month."'","Mes","assoc")[0]["Cuenta"];
                 $results2["Concepto"] = $this->mySql->select($table,["Concepto"],"Id_Cuenta = '".$account."'"." AND "."Mes = '".$month."'","Mes","assoc")[0]["Concepto"];
 
